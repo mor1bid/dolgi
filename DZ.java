fe2a844b11397a139fa3596b4bd8369d1e3ad9a7
De <spark49942@gmail.com>
2024-06-27 01:30:42 +0300

    rbtri-2

--- DZ.java
+++ DZ.java
@@ -29,7 +29,36 @@ class node
     }
 }
 
-public class DZ {
-    
+public class DZ 
+{
+    private static node root = null;
 
+    node goleft(node work) 
+    {
+        System.out.println("поворот налево\n");
+        node kid = work.right;
+        node moves = work.left;
+        kid.left = work;
+        work.right = moves;
+        return kid;
+    }
+
+    node goright(node work) 
+    {
+        System.out.println("поворот направо\n");
+        node kid = work.left;
+        node moves = work.right;
+        kid.right = work;
+        work.left = moves;
+        return kid;
+    }
+
+    boolean redcheck(node work) 
+    {
+        if (work == null) 
+        {
+            return false;
+        }
+        return (work.color == true);
+    }
 }
